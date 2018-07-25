64cf61359ac383686eb4990a13ddbb2a159c3710
quiiwi <kilic.aslan@lepoles.com>
2018-07-25 10:26:12 +0200

    c1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Task} from "../../models/task";
 
 @Component({
   selector: 'page-home',
@@ -11,4 +12,30 @@ export class HomePage {
 
   }
 
+  /** Création d'une tâche @type {task} */
+  task: Task = new Task(); /** task même données que Task et on crée une nouvelle Task() */
+
+  /** Liste des tâches @type {Task[]}  */
+  tasks: Task[] = [ /** tasks même données que Task[], qui est égal au tableau ci-dessous */
+    {
+      id:1,
+      title:'promener le chien',
+      status:false
+    },
+    {
+      id:2,
+      title:'Arroser les plantes',
+      status:true
+    },
+    {
+      id:3,
+      title:'mourrir',
+      status:false
+    },
+    {
+      id:4,
+      title:'être en dépression',
+      status:true
+    }
+  ];
 }
